96bee5de7e17c2874a1ae9364ebd111d1c1386f6
Eyebrock <eyebrock2913@gmail.com>
2024-09-25 14:36:55 -0600

    swiper fix

--- src/components/SwiperComponent.jsx
+++ src/components/SwiperComponent.jsx
@@ -10,35 +10,91 @@ import img from '../assets/images/black.png';
 
 
 // import required modules
-import { FreeMode, Pagination } from 'swiper/modules';
+import { FreeMode, Pagination, Autoplay, EffectCoverflow, EffectCards} from 'swiper/modules';
 
 export default function SwiperComponent() {
   return (
     <>
-      <Swiper
-        slidesPerView={3}
-        spaceBetween={30}
+      <Swiper effect={'coverflow'}
+        slidesPerView={4}
+        spaceBetween={5}
         freeMode={true}
         pagination={{
           clickable: true,
         }}
-        modules={[FreeMode, Pagination]}
+              coverflowEffect={{
+                rotate: 50,
+                stretch: 0,
+                depth: 100,
+                modifier: 1,
+                slideShadows: true,
+              }}
+        loop={true}
+        autoplay={
+            {
+                delay: 1000,
+                disableOnInteraction: false
+            }
+        }
+        modules={[FreeMode, Autoplay, EffectCoverflow]}
         className="mySwiper"
       >
+        {/*<SwiperSlide>*/}
+        {/*  <img src={img.src} alt="black" />*/}
+        {/*</SwiperSlide>*/}
+        {/*<SwiperSlide>*/}
+        {/*  <img src={img.src} alt="black" />*/}
+        {/*</SwiperSlide>*/}
+        {/*<SwiperSlide>*/}
+        {/*  <img src={img.src} alt="black" />*/}
+        {/*</SwiperSlide>*/}
+        {/*<SwiperSlide>*/}
+        {/*  <img src={img.src} alt="black" />*/}
+        {/*</SwiperSlide>*/}
+        {/*<SwiperSlide>*/}
+        {/*  <img src={img.src} alt="black" />*/}
+        {/*</SwiperSlide>*/}
+        {/*<SwiperSlide>*/}
+        {/*  <img src={img.src} alt="black" />*/}
+        {/*</SwiperSlide>*/}
+        {/*<SwiperSlide>*/}
+        {/*  <img src={img.src} alt="black" />*/}
+        {/*</SwiperSlide>*/}
+        {/*<SwiperSlide>*/}
+        {/*  <img src={img.src} alt="black" />*/}
+        {/*</SwiperSlide>*/}
+        {/*<SwiperSlide>*/}
+        {/*  <img src={img.src} alt="black" />*/}
+        {/*</SwiperSlide>*/}
+        {/*<SwiperSlide>*/}
+        {/*  <img src={img.src} alt="black" />*/}
+        {/*</SwiperSlide>*/}
         <SwiperSlide>
-          <img src={img.src} alt="black" />
+          <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
         </SwiperSlide>
         <SwiperSlide>
-          <img src={img.src} alt="black" />
+          <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
         </SwiperSlide>
         <SwiperSlide>
-          <img src={img.src} alt="black" />
+          <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
         </SwiperSlide>
         <SwiperSlide>
-          <img src={img.src} alt="black" />
+          <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
         </SwiperSlide>
         <SwiperSlide>
-          <img src={img.src} alt="black" />
+          <img src="https://swiperjs.com/demos/images/nature-5.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-6.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-7.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-8.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-9.jpg" />
         </SwiperSlide>
       </Swiper>
     </>
